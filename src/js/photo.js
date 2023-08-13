7c1e26a742b8a61d90b94dd3b923c7f6eb5c65ab
DoubledConG <992196871@qq.com>
2023-08-13 14:16:26 +0800

    refactor: 💡 change to an engineering project

--- src/js/photo.js
+++ src/js/photo.js
@@ -1,4 +1,34 @@
-var mySwiper = new Swiper('.swiper', {
+import Swiper from '../../node_modules/swiper/swiper-bundle.min.mjs'
+const imgUrls = [
+    './imgs/pic00.png',
+    './imgs/pic01.jpg',
+    './imgs/pic03.jpg',
+    './imgs/pic04.png',
+    './imgs/pic05.png',
+    './imgs/pic06.jpeg',
+    './imgs/pic07.png',
+    './imgs/pic08.jpg',
+    './imgs/pic09.jpeg',
+    './imgs/pic10.jpg'
+]
+
+const swiper = document.createElement('div')
+swiper.className = 'swiper'
+const swiperWrapper = document.createElement('div')
+swiperWrapper.className = 'swiper-wrapper'
+
+imgUrls.forEach(url => {
+    const swiperSlide = document.createElement('div')
+    swiperSlide.className = 'swiper-slide'
+    const img = document.createElement('img')
+    img.setAttribute('src', url)
+    swiperSlide.appendChild(img)
+    swiperWrapper.appendChild(swiperSlide)
+})
+swiper.appendChild(swiperWrapper)
+document.getElementById('photoContainer').appendChild(swiper)
+
+const swiperObj = new Swiper('.swiper', {
     effect: 'flip',
     autoplay: true,
     direction: 'horizontal',
@@ -6,3 +36,5 @@ var mySwiper = new Swiper('.swiper', {
     speed: 1000,
     preloadImages: false,
 })
+
+export default swiperObj
